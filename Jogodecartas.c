11d24f5b133694b8f499ff7806b7928c705ea3be
BAIANO6969 <marcossantosfsa06@gmail.com>
2025-02-10 20:46:50 +0000

    adicionei os char

---
+++ Jogodecartas.c
@@ -0,0 +1,25 @@
+#include <stdio>
+
+  int(main){
+    char opçao = 'Brasil'
+    char opçao = 'estados unidos'
+    char opçao = 'japão'
+    char opçao = 'espanha'
+    char opçao = 'jamaica'
+    char opcao = 'portugal'
+    char opcao = 'noruega'
+    char opcao = 'corea do sul'
+
+
+    char letra = A;
+    char letra = B;
+    char letra = C;
+    char letra = D;
+    char letra = E;
+    char letra = F;
+    char letra = G;
+    char letra = H;
+
+
+
+  }
