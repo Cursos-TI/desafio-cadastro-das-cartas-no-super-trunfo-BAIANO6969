066871c27818f0509b377f201b5be3cd4e140989
BAIANO6969 <marcossantosfsa06@gmail.com>
2025-03-12 00:20:25 -0300

    Update supertrunfo(INTERMEDIARIO).c

--- supertrunfo(INTERMEDIARIO).c
+++ supertrunfo(INTERMEDIARIO).c
@@ -10,9 +10,9 @@ typedef struct {
     int numPontosTuristicos;
     float densidadePopulacional;
     float pibPerCapita;
-} Cidade;
+} Cidade; // contém todos os atributos necessários para armazenar as informações de cada cidade.
 
-void exibirCidade(Cidade cidade) {
+void exibirCidade(Cidade cidade) { // é responsável por imprimir os dados de uma cidade formatados.
     printf("Estado: %s\n", cidade.estado);
     printf("Código: %s\n", cidade.codigo);
     printf("Nome da Cidade: %s\n", cidade.nomeCidade);
@@ -25,11 +25,11 @@ void exibirCidade(Cidade cidade) {
     printf("\n");
 }
 
-int main() {
+int main() { // criei duas instâncias da estrutura, uma para São Paulo e outra para o Rio de Janeiro
     Cidade carta1 = {"A", "A01", "São Paulo", 12325000, 1521.11, 699.28, 50, 8102.47, 56724.32};
     Cidade carta2 = {"B", "B02", "Rio de Janeiro", 6748000, 1200.25, 300.50, 30, 5622.24, 44532.91};
 
-    printf("Informações das Cidades:\n\n");
+    printf("Informações das Cidades:\n\n");// Por fim, chamei a função exibirCidade para mostrar as informações de ambas as cidades.
     exibirCidade(carta1);
     exibirCidade(carta2);
 
